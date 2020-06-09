34672e7fd84a32bac4a1bb8546003cb57580e907
pythoner <32189166@qq.com>
2020-06-09 16:06:49 +0800

    添加tableEdit组件

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,20 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+	<tableEdit></tableEdit>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import tableEdit from './components/tableEdit.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+	tableEdit
+	
   }
 }
 </script>

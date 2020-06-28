fd1b95a6d3ef7edd7c582b9ee1ec9a80c04d60d6
pythoner <32189166@qq.com>
2020-06-28 16:39:32 +0800

    添加coloredit组件

---
+++ src/components/colorEdit.vue
@@ -0,0 +1,26 @@
+<template>
+	<div>
+		<colorPicker v-model='color' @change='changcolor'></colorPicker>
+		<!-- <button @change="changcolor">更多颜色1</button> -->
+		<slot></slot>
+	</div>
+	
+</template>
+
+<script>
+export default {
+	name:'colorEdit',
+	data() {
+		return {
+			color: '#000000'		}
+	},
+	methods: {
+		changcolor(val) {
+			console.log(val);
+		}
+	},
+}
+</script>
+
+<style>
+</style>

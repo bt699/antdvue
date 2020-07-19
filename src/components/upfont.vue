f2bd672a13a8861cedd8bde484e980df551322e0
pythoner <32189166@qq.com>
2020-07-19 15:47:13 +0800

    upfont组件

---
+++ src/components/upfont.vue
@@ -0,0 +1,11 @@
+<template>
+	<a-upload action="http://127.0.0.1:5000/updatafont" directory>
+	    <a-button> <a-icon type="upload" /> Upload Directory </a-button>
+	  </a-upload>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
